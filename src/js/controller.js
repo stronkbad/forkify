ac6a5329eeb9c31f7a3f331ff83b09741a58e83c
stronkbad <aroydupuis@gmail.com>
2023-04-20 22:50:56 -0400

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -108,6 +108,11 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
+
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -116,6 +121,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome to the application!');
+  newFeature();
 };
 init();
